235ef65a6b4feb9e3d5a2288356fcc3b171e3f55
Jaswanth-Kumar-2007 <jaswanthkumarkamireddi@gmail.com>
2026-08-20 23:21:55 +0530

    Made Some Issues

---
+++ C-Basics/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    printf("Welcome to C Basics!\n");
+
+    printf("Addition: %d\n", add(a, b));
+    printf("Subtraction: %d\n", subtract(a, b));
+    printf("Multiplication: %d\n", multiply(a, b));
+
+    printf("Division: %d\n", divide(a, b)
+
+    return 0;
+}
---
+++ C-Basics/student.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50] = "OpenLake";
+    int age = 18
+
+    printf("Student: %s\n", name);
+    printf("Age: %d\n", age);
+
+    return 0;
+}
