0fd73104638405bc76b7f0090410c31abfebeabd
Archit <architpasayat28@gmail.com>
2026-08-21 22:24:35 +0530

    Add files via upload

---
+++ Python-Basics/Factorial.py
@@ -0,0 +1,10 @@
+def factorial(n: int) -> int:
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n):
+        result *= i
+    return result
+
+input = int(input())
+print(f"The factorial of {input} is: {factorial(input)}")
